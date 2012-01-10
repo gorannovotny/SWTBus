78bb37ee7976c86107e6daefa6397061ea76d5a3
Goran Novotny <goran@abit.hr>
2012-01-10 14:22:22 +0100

    Dodana baza

--- src/gogo/DbUtil.java
+++ src/gogo/DbUtil.java
@@ -13,7 +13,7 @@ public class DbUtil {
 		if (con == null) {
 			try {
 				Class.forName("org.sqlite.JDBC");
-				con = DriverManager.getConnection("jdbc:sqlite:/home/goran/work/MitMobile/sqlite.db");
+				con = DriverManager.getConnection("jdbc:sqlite:sqlite.db");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
